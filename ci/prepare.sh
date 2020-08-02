8c9bdff004a5adeddcb899cbefb54f10aa0334db
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-08-02 20:22:43 +0200

    download gnulib from github directly as zipfile

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -6,10 +6,17 @@
 # Released under GNU General Public License, either version 3
 # or any later option
 
+# Assume that we are in the pspp git source directory
+if ! test -f ./Smake; then
+    echo "I am not in the pspp git source directory - exiting"
+    exit 1
+fi
+
 cd ..
 #Download gnulib
-git clone https://git.savannah.gnu.org/git/gnulib.git
-cd gnulib
-git checkout 1e972a8a37c153ddc15e604592f84f939eb3c2ad
-cd ..
+gnulibver=1e972a8a37c153ddc15e604592f84f939eb3c2ad
+curl -o gnulib.zip https://codeload.github.com/coreutils/gnulib/zip/$gnulibver
+unzip -q gnulib.zip
+rm gnulib.zip
+mv gnulib-$gnulibver gnulib
 
