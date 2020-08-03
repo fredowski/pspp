2cec7bcddc1ef3452352cba88afb8e234458d5a0
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-08-03 12:46:59 +0200

    seperate make and make check

--- ci/buildpspp-linux.sh
+++ ci/buildpspp-linux.sh
@@ -10,8 +10,6 @@ bundleinstall=/usr
 # The source tree for pspp is here.
 psppsource=`pwd`
 
-make -f Smake
-
 # Retrieve and Set Version Info
 gitversion=`git log --pretty=format:"%h" -1`
 repoversion=`sed -n 's/AC_INIT.*\[\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/p' configure.ac`
@@ -23,8 +21,3 @@ mkdir ./build
 cd build
 $psppsource/configure --prefix=$bundleinstall
 make VERSION=$psppversion
-make check
-make html
-sudo make install
-sudo make install-html
-cd ..
